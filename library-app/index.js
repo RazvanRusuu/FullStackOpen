9c9842d6379390bee1bee0828c8b5664134afe5e
Razvan Rusu <razvanrusu9420@gmail.com>
2024-03-19 07:17:21 +0200

    continue apollo server challanges

--- library-app/index.js
+++ library-app/index.js
@@ -1,5 +1,7 @@
-const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
+const { ApolloServer } = require("@apollo/server");
+const { GraphQLError } = require("graphql");
+const { v1: uuid } = require("uuid");
 
 let authors = [
   {
@@ -83,8 +85,28 @@ const typeDefs = `
     type Query {
         bookCount: Int!
         authorCount: Int!
-        allBooks: [Book!]!
+        allBooks(author: String, genre: String): [Book!]!
         allAuthors: [Author!]!
+    }
+
+    type Mutation {
+        addBook(
+            title:String!
+            author: String!
+            published: Int!
+            genres: [String!]!
+        ): Book
+        editAuthorYear(
+            name: String!
+            setBornTo: Int!
+        ): Author
+    }
+
+    type Author {
+        name: String!
+        born: Int
+        id: ID!
+        bookCount: Int!
     }
 
     type Book {
@@ -95,33 +117,75 @@ const typeDefs = `
         genres: [String!]!
     }
 
-    type Author {
-        name: String!
-        id: ID!
-        born: Int
-        bookCount: Int
-    }
+
 `;
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author && !args.genre) return books;
+
+      let filteredBook = [...books];
+
+      if (args.author)
+        filteredBook = filteredBook.filter((b) => b.author === args.author);
+
+      if (args.genre) {
+        filteredBook = filteredBook.filter((b) =>
+          b.genres.includes(args.genre)
+        );
+      }
+
+      return filteredBook;
+    },
     allAuthors: () => authors,
   },
+  Mutation: {
+    addBook: (root, args) => {
+      if (!args.title && !args.author && !args.published && !args.genres) {
+        throw new GraphQLError("Fields are mandatory", {
+          extensions: {
+            code: "BAD_USER_INPUT",
+            invalidArgs: "",
+          },
+        });
+      }
+      const existingAuthor = authors.find((a) => a.name === args.author);
+      if (!existingAuthor)
+        authors = [...authors, { name: args.author, id: uuid() }];
 
+      const newBook = { ...args, id: uuid() };
+      books = [...books, newBook];
+      return newBook;
+    },
+    editAuthorYear: (root, args) => {
+      if (!args.name) return;
+
+      const author = authors.find((a) => a.name === args.name);
+
+      if (!author) return;
+
+      const updatedAuthor = { ...author, born: args.setBornTo };
+
+      authors = authors.map((a) => (a.name === args.name ? updatedAuthor : a));
+
+      return updatedAuthor;
+    },
+  },
   Author: {
     bookCount: (root) => {
-      return books.filter((b) => b.author === root.name).length;
+      return books.filter((b) => root.name === b.author).length;
     },
   },
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
 startStandaloneServer(server, {
   listen: { port: 4000 },
-}).then(({ url }) => {
-  console.log(`Server ready at ${url}`);
-});
+}).then(({ url }) => console.log(`Server started at ${url}`));
